c15031c6b35df4a7197910343cf27b192302c018
Evan Anderson <evan.anderson@caveon.com>
2017-10-02 16:41:14 -0600

    creating deliveries and sending emails

--- server.js
+++ server.js
@@ -6,11 +6,24 @@ const bodyParser = require('body-parser');
 const config = require('./config.json');
 const request = require('request');
 const jwt = require('jsonwebtoken');
+const mailer = require('express-mailer');
 var PersistentStorage = require('./repository');
-
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mailer.extend(app, {
+    from: config.MAIL_USERNAME,
+    host: config.MAIL_SERVER,
+    secureConnection: false,
+    port: 587,
+    transportMethod: 'SMTP',
+    auth: {
+        user: config.MAIL_USERNAME,
+        pass: config.MAIL_PASSWORD
+    }
+});
 
 var db = new PersistentStorage();
 
@@ -43,15 +56,90 @@ app.get('/authorize', function (req, res) {
 app.get('/main', function (req, res) {
     var examId = req.query.exam_id;
     var token = req.query.jwt;
+    if (!examId || !token) {
+        res.sendStatus(500);
+    }
+
     db.get(examId).then(function (integrationInfo) {
         try {
             jwt.verify(token, integrationInfo.secret);
+            request.get(config.SEI_BASE + '/api/exams/' + integrationInfo.exam_id + '?include=settings',
+            { 'auth': { 'bearer': integrationInfo.token } }, function (error, response, body) {
+                var exam = JSON.parse(body);
+                var examineeSchema = exam.settings.examinee_schema;
+                console.log('exam', examineeSchema);
+                res.render('pages/main', { examineeSchema: examineeSchema });
+            });
         } catch (e) {
-            
+            console.log(e);
+            res.sendStatus(500);
         }
-        res.render('pages/main');
     });
 });
+
+app.post('/main', function (req, res) {
+    var examId = req.query.exam_id;
+    var token = req.query.jwt;
+
+    if (!examId || !token) {
+        res.sendStatus(500);
+    }
+
+    db.get(examId).then(function (integrationInfo) {
+        try {
+            jwt.verify(token, integrationInfo.secret);
+            var examineeInfos = [];
+            var keys = Object.keys(req.body);
+            for (var i = 0; i < req.body.emails.length; i++) {
+                var info = {};
+                var email = null;
+                for (var j = 0; j < keys.length; j++) {
+                    var currentKey = keys[j];
+                    if (currentKey === 'emails') {
+                        email = req.body[currentKey][i];
+                    } else {
+                        info[currentKey] = req.body[currentKey][i];
+                    }
+                }
+                
+                var deliveryPostData = {
+                    'method': 'POST',
+                    'headers': {
+                        'Authorization': 'Bearer ' + integrationInfo.token
+                    },
+                    'url': config.SEI_BASE + '/api/exams/' + integrationInfo.exam_id + '/deliveries',
+                    'json': { 'examinee_info' : info }
+                };
+                createDelivery(deliveryPostData, email);
+                examineeInfos.push(info);
+            }
+            res.render('pages/emailSuccess');
+        } catch (e) {
+            console.log(e);
+            res.sendStatus(500);
+        }
+    });
+});
+
+function createDelivery(deliveryPostData, email) {
+    request(deliveryPostData, function (error, response, body) {
+        var takeUrl = String(config.TAKE_URL + '?launch_token=' + body.launch_token);
+        console.log('send email to:', email);
+        console.log('with content:', takeUrl);
+
+        app.mailer.send('emails/deliveryLink', {
+            to: email,
+            subject: 'New Delivery',
+            deliveryLink: takeUrl
+        }, function (err) {
+            if (err) {
+                // handle error 
+                console.log(err);
+            }
+            console.log('Email Sent');
+        });
+    });
+}
 
 
 const httpsOptions = {
